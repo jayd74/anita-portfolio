eccce7aaf528ec2259202e87d0c75ac8eea59aa9
Jason Du <jayd74@gmail.com>
2020-11-11 23:24:18 -0500

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <g-image src="~/layouts/assets/logo.svg" />
+
+      <nav>
+        <ul>
+          <li>work</li>
+          <li>art</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+nav ul {
+  display: flex;
+}
+</style>
